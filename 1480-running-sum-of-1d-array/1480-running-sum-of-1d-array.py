2fdd0e3bb668f068e9b784be975804851e01644b
James Astell <45542564+AstellJ11@users.noreply.github.com>
2023-05-14 12:54:06 +0100

    Time: 23 ms (73.06%), Space: 13.4 MB (87.55%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.py
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.py
@@ -8,9 +8,8 @@ class Solution(object):
         running_sum = list()
         previous_sum = 0
         
-        for i in range(len(nums)):
-            current_sum = nums[i]     
-            running_sum.append(previous_sum + current_sum)
-            previous_sum = previous_sum + current_sum
+        for i in range(len(nums)):    
+            running_sum.append(previous_sum + nums[i])
+            previous_sum = previous_sum + nums[i]
         
         return running_sum
